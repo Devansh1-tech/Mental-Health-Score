491f3d3d875714c97213f28db199a81cdfbc46c2
Devansh1-tech <guptadevansh470@gmail.com>
2026-07-27 09:36:00 +0530

    updated deploy link backened

--- frontend/src/components/PredictionForm.jsx
+++ frontend/src/components/PredictionForm.jsx
@@ -159,7 +159,7 @@ export default function PredictionForm({ onResult, onError, loading, setLoading 
     setLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/predict', {
+      const response = await fetch("https://mental-health-score-5v35.onrender.com/predict",{
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
